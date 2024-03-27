9cc6cc2b1917ae0b7f30eee914ce48cff51e573b
AGA2020 <hasan9u@gmail.com>
2024-03-27 23:42:29 +0300

    Proje bitti

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int n1,n2,select;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+        System.out.print("İlk Sayıyı Giriniz : ");
+        n1 = input.nextInt();
+        System.out.print("İkinci Sayıyı Giriniz : ");
+        n2 = input.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz : ");
+        select = input.nextInt();
+
+        switch(select) {
+            case 1:
+                System.out.println("Toplam : " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Çıkarma : " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Çarpma : " + (n1 * n2));
+                break;
+            case 4:
+                if (n2 == 0) {
+                    System.out.println("Bir sayı 0'a bölünemez!");
+                } else {
+                    System.out.println("Bölme : " + (n1 / n2));
+                }
+                break;
+            default:
+                System.out.println("Yanlış seçim yaptınız. Tekrar deneyiniz.");
         }
     }
 }
